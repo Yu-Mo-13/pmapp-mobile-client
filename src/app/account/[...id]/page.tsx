87ce437b022be70dc1c4494fe48cf178d29eb85d
Yuya Motozono <motozono.tech@gmail.com>
2024-08-31 15:15:21 +0900

    mod:account detail

---
+++ src/app/account/[...id]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "@/app/page.module.css";
+
+export default function Page() {
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.title}>Hello, world!</h1>
+    </div>
+  );
+}
