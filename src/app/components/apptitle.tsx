e1ad951f187b8634cf5934bd84561ab49e1501c6
Yuya Motozono <motozono.tech@gmail.com>
2024-09-01 13:59:35 +0900

    checkpoint:login

--- src/app/components/apptitle.tsx
+++ src/app/components/apptitle.tsx
@@ -6,8 +6,8 @@ export const AppTitle = (props: AppTitleProps) => {
 
   const captionStyle: CSS.Properties = {
     width: "100%",
-    height: "3rem",
-    fontSize: "1.5rem",
+    height: "4rem",
+    fontSize: "1.4rem",
     textAlign: "center",
   };
 
