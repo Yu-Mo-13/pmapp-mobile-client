558c4f17fecea487a8452a9ea7c281c35b755805
Yuya Motozono <motozono.tech@gmail.com>
2024-09-01 13:20:44 +0900

    mod:login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,67 @@
-import Image from "next/image";
+"use client";
+import React, { use } from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import * as yup from "yup";
 import styles from "./page.module.css";
+import { AppTitle } from './components/apptitle';
+import { Textbox } from "./components/textbox/textbox";
+import { LargeButton } from "./components/button/large";
+
+const loginSchema = yup.object().shape({
+  userId: yup.string().required("ログイン情報を入力してください。"),
+  keyword: yup.string().required("ログイン情報を入力してください。"),
+});
 
 export default function Home() {
+  const [userId, setUserId] = useState<string>("");
+  const [keyword, setKeyword] = useState<string>("");
+  const router = useRouter();
+
+  const onClickLogin = async () => {
+    try {
+      await loginSchema.validate({ userId, keyword });
+      router.push("/menu");
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        // バリデーションエラー
+        alert(error.message);
+        return;
+      }
+      if (error instanceof TypeError) {
+        // ログインエラー
+        alert("ユーザーIDまたはパスワードが間違っています。");
+      }
+      return;
+    }
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className={styles.content}>
+        <AppTitle caption="PMAPP Mobile"/>
+        {/* ユーザーIDを入力するテキストボックス */}
+        <Textbox
+          type="text"
+          id="USERID"
+          placeholder="ユーザーID"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUserId(e.target.value)
+          }
+          val={userId}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {/* ユーザーIDを入力するテキストボックス */}
+        <Textbox
+          type="password"
+          id="PASSWORD"
+          placeholder="パスワード"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setKeyword(e.target.value)
+          }
+          val={keyword}
+        />
+        {/* ログインボタン */}
+        <LargeButton caption="ログイン" onClick={onClickLogin} isEnabled={true} />
       </div>
     </main>
   );
